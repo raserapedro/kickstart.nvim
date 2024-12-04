81bbb66180d7293d594cb9890f109830cb3abde3
Pedro Rasera <porasera@uolinc.com>
2024-12-04 16:54:25 -0300

    add OpenModifiedFiles custom command

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,2 +1,10 @@
 require 'custom.remap'
 vim.g.codeium_disable_bindings = 1
+
+function OpenModifiedFiles()
+  local modified_files = vim.fn.systemlist 'git diff --name-only'
+  for _, file in ipairs(modified_files) do
+    vim.cmd('edit ' .. file)
+  end
+end
+vim.api.nvim_create_user_command('OpenModified', OpenModifiedFiles, {})
