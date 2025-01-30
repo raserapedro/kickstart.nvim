56ef303b03adb38241def40bf7015c2021723a8f
Pedro Rasera <porasera@uolinc.com>
2025-01-30 11:17:27 -0300

    add keymap to delete buffer from telescope buffer select

--- init.lua
+++ init.lua
@@ -382,12 +382,15 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          mappings = {
+            i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+            n = {
+              ['dd'] = require('telescope.actions').delete_buffer,
+            },
+          },
+        },
+        pickers = {},
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
