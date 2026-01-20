1a3515bab71a9de4e8c03e1e4e472e6f7495dff0
Pedro Rasera <pedro.rasera@capim.com.br>
2026-01-20 17:01:38 -0300

    feat: more keybinds

--- lua/custom/keybinds/keybinds.lua
+++ lua/custom/keybinds/keybinds.lua
@@ -7,4 +7,6 @@ return {
   vim.keymap.set('n', '<leader>bv', '<CMD>vsplit<CR>', { desc = '[B]uffers [V]ertical split' }),
   vim.keymap.set('n', '<leader>bh', '<CMD>split<CR>', { desc = '[B]uffers [H]orizontal split' }),
   vim.keymap.set('n', '<leader>rn', '<CMD>set rnu!<CR>', { desc = 'Toggle [R]elative [N]umbers' }),
+  vim.keymap.set('n', '<leader>bcp', "<CMD>let @+ = expand('%')<CR>", { desc = '[B]uffers: [C]opy Relative [P]ath' }),
+  vim.keymap.set('n', '<leader>bb', '<C-^>', { desc = '[B]uffers: Previous buffer' }),
 }
